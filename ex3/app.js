e3b1907d32e4f69e8266f655e7213f2f88732eb0
A-Aswathi <aswathi7219@gmail.com>
2024-09-24 10:52:42 +0530

    Update app.js

--- ex3/app.js
+++ ex3/app.js
@@ -12,7 +12,8 @@ document.getElementById("postForm").addEventListener("submit", function (e) {
         image: imageFile ? URL.createObjectURL(imageFile) : null,
         likes: 0,
         dislikes: 0,
-        comments: []
+        comments: [],
+        dateTime: new Date().toLocaleString() // Date and time
     };
 
     posts.push(post);
@@ -30,10 +31,19 @@ function renderPosts() {
         postElement.innerHTML = `
             <p>${post.content}</p>
             ${post.image ? `<img src="${post.image}" alt="Post Image">` : ""}
+            <div class="date-time">${post.dateTime}</div>
             <div class="likes">Likes: <span id="like-count-${post.id}">${post.likes}</span> | 
             Dislikes: <span id="dislike-count-${post.id}">${post.dislikes}</span></div>
             <button onclick="likePost(${post.id})">Like</button>
             <button onclick="dislikePost(${post.id})">Dislike</button>
+
+            <div class="emoji-reactions">
+                <span class="emoji" onclick="addEmoji(${post.id}, '👍')">👍</span>
+                <span class="emoji" onclick="addEmoji(${post.id}, '❤️')">❤️</span>
+                <span class="emoji" onclick="addEmoji(${post.id}, '😂')">😂</span>
+                <span class="emoji" onclick="addEmoji(${post.id}, '😢')">😢</span>
+                <span class="emoji" onclick="addEmoji(${post.id}, '😡')">😡</span>
+            </div>
 
             <div class="comments">
                 <h4>Comments:</h4>
@@ -65,7 +75,8 @@ function addComment(postId) {
     const comment = {
         content: commentContent,
         likes: 0,
-        dislikes: 0
+        dislikes: 0,
+        dateTime: new Date().toLocaleString() // Date and time for comment
     };
 
     posts[postId].comments.push(comment);
@@ -81,6 +92,7 @@ function renderComments(postId) {
         commentElement.classList.add("comment");
         commentElement.innerHTML = `
             <p>${comment.content}</p>
+            <div class="date-time">${comment.dateTime}</div>
             <div class="comment-actions">
                 <div>Likes: <span id="comment-like-count-${postId}-${index}">${comment.likes}</span> | 
                 Dislikes: <span id="comment-dislike-count-${postId}-${index}">${comment.dislikes}</span></div>
@@ -92,6 +104,14 @@ function renderComments(postId) {
         `;
         commentsContainer.appendChild(commentElement);
     });
+}
+
+function addEmoji(postId, emoji) {
+    const emojiReactions = document.createElement('span');
+    emojiReactions.innerText = emoji;
+    emojiReactions.style.fontSize = "20px";
+    emojiReactions.style.marginRight = "5px";
+    document.querySelector(`#postsContainer > div:nth-child(${parseInt(postId) + 1}) .emoji-reactions`).appendChild(emojiReactions);
 }
 
 function likeComment(postId, commentIndex) {
